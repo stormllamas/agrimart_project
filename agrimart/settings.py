d3648215f22dd9df1d2754069323d478e39a515a
stormllamas <storm_sia_llamas@yahoo.com>
2020-11-02 18:44:25 +0800

    Dynamic DB config

--- agrimart/settings.py
+++ agrimart/settings.py
@@ -69,19 +69,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'agrimart.wsgi.application'
 
-# DATABASES = {
-#   'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': config('DB_NAME'),
-#     'USER': config('DB_USER'),
-#     'PASSWORD': config('DB_PASSWORD'),
-#     'HOST': config('DB_HOST'),
-#   }
-# }
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': 'agrimartdb.sqlite3',
+    'ENGINE': config('DB_ENGINE'),
+    'NAME': config('DB_NAME'),
+    'USER': config('DB_USER'),
+    'PASSWORD': config('DB_PASSWORD'),
+    'HOST': config('DB_HOST'),
   }
 }
 
