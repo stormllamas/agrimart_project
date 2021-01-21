ad9d4f9b16ad03225e3cdb99bd4f7a8c6ed1c40d
Storm <storm_sia_llamas@yahoo.com>
2021-01-21 12:47:15 +0800

    Remove browseable api

--- agrimart/settings.py
+++ agrimart/settings.py
@@ -139,15 +139,14 @@ DEFAULT_RENDERER_CLASSES = (
   'rest_framework.renderers.JSONRenderer',
 )
 
-# if DEBUG == True:
-#   DEFAULT_RENDERER_CLASSES = DEFAULT_RENDERER_CLASSES + (
-#     'rest_framework.renderers.BrowsableAPIRenderer',
-#   )
+if DEBUG:
+  DEFAULT_RENDERER_CLASSES = DEFAULT_RENDERER_CLASSES + (
+    'rest_framework.renderers.BrowsableAPIRenderer',
+  )
 
 REST_FRAMEWORK = {
-  # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-  # 'PAGE_SIZE': 100
-  'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+  'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
+  'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
 }
 
 REST_KNOX = {
