64a756166b7a79988a7814d6d07dc6436b87d74f
Storm <storm_sia_llamas@yahoo.com>
2021-01-21 11:24:26 +0800

    Add statcounter

--- frontend/src/actions/pages.js
+++ frontend/src/actions/pages.js
@@ -33,7 +33,7 @@ export const setPage = (page, history) => async dispatch => {
 
 export const updateQuery = history => async (dispatch, getState) => {
   let pagePath = ''
-  const { currentPage } = getState().manager;
+  const { currentPage } = getState().articles;
 
   // Set Paths
   if (currentPage) pagePath = `page=${currentPage}`
